2c537a20b8a51796eadd61615e38d6d72e36ffe1
BramsuryaJP <bramsuryajohannespaulus.work@gmail.com>
2022-07-06 09:58:31 +0700

    Menambahkan Komentar

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,6 +1,9 @@
 
 console.info("Hello World");
 
+// memasukan nama kedalam variable name
 const name = "Bramsurya Johannes Paulus";
 
+//memanggil variable name 
 console.info(`Hello my name is ${name}`);
+
